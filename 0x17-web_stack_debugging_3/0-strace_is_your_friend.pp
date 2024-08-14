061fbc3015f6f8aed5716fc185d5471e62037654
hima890 <hfibrahim90@gmail.com>
2024-08-14 17:12:56 +0200

    addTask

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Creat a manifest that fix all termintion of php server.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
+
